1dfca11e9bea2e79fb0df414a6780feac91298f9
Pablo Contreras <pacg1991@gmail.com>
2021-07-11 13:42:23 -0500

    skills

--- src/views/admin/Skills.vue
+++ src/views/admin/Skills.vue
@@ -5,53 +5,17 @@
                 <Title :text="title"/>
             </h5>
             <div class="row">
-                <div class="col-6 d-flex" v-for="(value, index) in skills" :key="index">
-                    <div class="row col-11" v-if="!value.editing" v-on:click="value.editing = true">
-                        <div class="col-12">
-                            <div class="row">
-                                <i :class="[value.icon]+' col-2 theme-color-'+[theme]+'-800 p-2'" style="font-size: 1rem;"></i>
-                                <div class="col-10">{{ value.text }}</div>
-                                <div class="progress col-12" style="height: 3px;">
-                                    <div :class="'progress-bar theme-bg-'+[theme]+'-800'" role="progressbar" 
-                                        :style="{
-                                            width: `${value.percentage}%`
-                                        }"
-                                    >
-                                    </div>
-                                </div>
-                            </div>
+                <div class="col-6 row" v-for="(value, index) in skills" :key="index">
+                    <i :class="[value.icon]+' col-2 theme-color-'+[theme]+'-800 p-2'" style="font-size: 1rem;"></i>
+                    <div class="col-10">{{ value.text }}</div>
+                    <div class="progress col-12" style="height: 3px;">
+                        <div :class="'progress-bar theme-bg-'+[theme]+'-800'" role="progressbar" 
+                            :style="{
+                                width: `${value.percentage}%`
+                            }"
+                        >
                         </div>
                     </div>
-
-                    <div
-                        class="row col-11" 
-                        v-if="value.editing"
-                    >
-                        <i :class="[value.icon]+' col-2 theme-color-'+[theme]+'-800 p-2'" style="font-size: 1rem;"></i>
-                        <input class="form-control col-8" v-model="value.icon"
-                            placeholder="Icon"
-                            v-on:keyup.enter.exact="value.editing = false"
-                            v-on:keyup.esc="value.editing = false"
-                        >
-                        <input class="form-control m-1 col-10" v-model="value.text"
-                            placeholder="Skill"
-                            v-on:keyup.enter.exact="value.editing = false"
-                            v-on:keyup.esc="value.editing = false"
-                        >
-                        <input type="range" class="form-range m-1 col-10" min="0" max="100" step="0.5" v-model="value.percentage">
-
-                    </div>
-
-                    <div class="col-1">
-                        <button type="button" v-on:click="remove(index)" :class="'btn theme-bg-'+[theme]+'-200'">
-                            <i :class="'fas fa-times'"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="pt-2 d-flex justify-content-center">
-                    <button type="button" v-on:click="add" :class="'btn theme-bg-'+[theme]+'-200'">
-                        <i :class="'fas fa-plus'"></i>
-                    </button>
                 </div>
             </div>
         </div>
@@ -152,21 +116,6 @@ export default {
     },
     components: {
         Title,
-    },
-    methods:{
-        add() {
-            this.skills.push(
-                {
-                    icon: '',
-                    text: '',
-                    percentage: 0,
-                    editing: true,
-                },
-            );
-        },
-        remove(i) {
-            this.skills.splice(i,1);
-        },
     },
 }
 </script>
