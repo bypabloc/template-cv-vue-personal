d85ba29d8519d6d8c071faabb218ff9df6ebe9db
Pablo Contreras <pacg1991@gmail.com>
2021-07-11 13:35:15 -0500

    Iam

--- src/views/admin/Iam.vue
+++ src/views/admin/Iam.vue
@@ -5,21 +5,9 @@
             <h5 class="card-title">
                 <Title :text="title"/>
             </h5>
-            <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
             <div class="card-text">
                 <div v-for="(value, index) in descriptions" :key="index">
-                    <p v-html="value.text" v-if="!value.editing" v-on:click="value.editing = true"></p>
-                    <textarea 
-                        v-if="value.editing" 
-                        v-model="value.text"
-                        v-on:keyup="resizeTextarea($event)"
-                        v-on:keydown="resizeTextarea($event)"
-                        v-on:keyup.enter.exact="value.editing = false"
-                        v-on:keyup.esc="value.editing = false"
-                        v-on:blur="value.editing = false"
-                        class="form-control" 
-                        placeholder="Descripcion de ti"
-                    ></textarea>
+                    <p v-html="value.text"></p>
                 </div>
             </div>
         </div>
